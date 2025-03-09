6c382a64d64aa0dab9cbd16b4e171bfcaa124b0e
Chris Board <support@boardiesitsolutions.com>
2025-03-09 20:46:56 +0000

    Remove hardcoded arguments from start script
    This update ensures the `start` script in `package.json` no longer includes hardcoded arguments. These previously hardcoded arguments should now be passed in as environment variables or command line arguments when calling the start script.

--- bin/index.ts
+++ bin/index.ts
@@ -4,21 +4,9 @@ import * as fs from 'fs';
 import {FileNotFoundException} from "./exceptions/FileNotFoundException";
 import {ResultFileEmptyException} from "./exceptions/ResultFileEmptyException";
 import axios from "axios";
-import * as console from "console";
 import {InvalidDataException} from "./exceptions/InvalidDataException";
-import * as Sentry from "@sentry/node";
-import { ProfilingIntegration } from "@sentry/profiling-node";
-
-Sentry.init({
-    dsn: "https://f70bc503be4cf0bcc9ffc284ecf42bc6@o4504350151081984.ingest.sentry.io/4506503849967616",
-    integrations: [
-        new ProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-});
+import {argv} from "yargs";
+import * as process from "process";
 
 const yargs = require("yargs");
 
@@ -29,7 +17,7 @@ const fileToBase64 = (filePath: string) => {
 }
 
 const sendRequest = async (data: any) => {
-    const url = 'http://test-track.home.com/backend/api/automated-test-run';
+    const url = 'https://app.test-track.com/backend/api/automated-test-run';
 
     const headers = {
         'Authorization': `Bearer ${apiKey}`,
@@ -50,6 +38,7 @@ const sendRequest = async (data: any) => {
 
 const options = yargs
     .usage("Usage: --api-key <API Key> --project-id <Project ID>")
+    .option("version", {alias: "v", description: "Show version number", type: "boolean"})
     .option("key", { alias: "api-key", describe: "Company API Key", type: "string", demandOption: true })
     .option("id", {alias: "project-id", describe: "Project id you are submitting the automated test run against", type: "string", demandOption: true})
     .option("file", {alias: "file", description: "JUnit XML File path", demandOption: true})
@@ -59,10 +48,9 @@ const apiKey = options['api-key'];
 const projectId = options['project-id'];
 const file = options.file;
 
-
-
 try
 {
+
     if (apiKey.length === 0)
     {
         throw new InvalidDataException("api-key");
@@ -105,11 +93,17 @@ try
         {
             console.log(`Received unexpected response code: ${response.status}: ${response.statusText}`);
             console.log("If you continue to see this, please raise a support ticket at https://support.devso.io and include the information below");
-            console.log(`Status Code: ${response.status} - ${response.statusText}`);
-            console.log(response.data);
+            if (typeof response?.status !== typeof undefined && typeof response?.statusText !== typeof undefined) {
+                console.log(`Status Code: ${response.status} - ${response.statusText}`);
+            }
+            if (typeof response?.data !== typeof undefined)
+            {
+                console.log(response.data);
+            }
+
         }
     }).catch(err => {
-        console.error("An unexpected error occurred submitting the automated test run to Test Track");
+        console.error("An unexpected error occurred submitting the automated test run to Test Track", err);
         console.error("If you continue to see this problem, please raise a support ticket at https://support.devso.io with the details outputted below")
         console.error("Status: " + err.response.status + ": " + err.response.statusText);
         console.error("Err", err.response.data);
